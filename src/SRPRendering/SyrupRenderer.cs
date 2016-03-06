05a3e56e0c03f4f0903ce68a404f59b96eb78938
Simon Taylor <simontaylor81@hotmail.com>
2016-03-06 18:46:23 +0100

    Cleanup

--- src/SRPRendering/SyrupRenderer.cs
+++ src/SRPRendering/SyrupRenderer.cs
@@ -18,7 +18,6 @@ namespace SRPRendering
 	{
 		public SyrupRenderer(IWorkspace workspace, RenderDevice device)
 		{
-			_workspace = workspace;
 			_device = device;
 
 			// Create object for interacting with script.
@@ -48,9 +47,6 @@ namespace SRPRendering
 
 					// Create new one.
 					_renderScene = new RenderScene(_scene, _device);
-
-					// Missing scene can cause rendering to fail -- give it another try with the new one.
-					bScriptRenderError = false;
 				}
 			}
 		}
@@ -66,55 +62,17 @@ namespace SRPRendering
 
 		public void Render(DeviceContext deviceContext, ViewInfo viewInfo)
 		{
-			// Bail if there was a problem with the scripts.
-			if (HasScriptError)
-			{
-				return;
-			}
+			// Always clear the back buffer to black to avoid the script having to do so for trivial stuff.
+			deviceContext.ClearRenderTargetView(viewInfo.BackBuffer, new RawColor4());
 
-			try
-			{
-				// Always clear the back buffer to black to avoid the script having to do so for trivial stuff.
-				deviceContext.ClearRenderTargetView(viewInfo.BackBuffer, new RawColor4());
-
-				// Let the script do its thing.
-				_scriptRenderControl.Render(deviceContext, viewInfo, _renderScene);
-			}
-			catch (Exception)
-			{
-				// Remember that the script fails so we don't just fail over and over.
-				bScriptRenderError = true;
-
-				// TODO: How do we handle exceptions here?
-				throw;
-			}
-
-			// Make sure we're rendering to the back buffer before rendering the overlay.
-			deviceContext.OutputMerger.SetTargets(viewInfo.DepthBuffer.DSV, viewInfo.BackBuffer);
-			deviceContext.Rasterizer.SetViewports(new[] { new RawViewportF
-			{
-				X = 0.0f,
-				Y = 0.0f,
-				Width = viewInfo.ViewportWidth,
-				Height = viewInfo.ViewportHeight,
-				MinDepth = 0.0f,
-				MaxDepth = 0.0f,
-			} });
+			// Let the script do its thing.
+			_scriptRenderControl.Render(deviceContext, viewInfo, _renderScene);
 		}
 
 		// Wrapper class that gets given to the script, acting as a firewall to prevent it from accessing this class directly.
 		public IRenderInterface ScriptInterface { get; }
 
 		private RenderDevice _device;
-
-		// Pointer back to the workspace. Needed so we can access the project to get shaders from.
-		private IWorkspace _workspace;
-
-		private bool bScriptExecutionError = false;		// True if there was a problem executing the script
-		private bool bScriptRenderError = false;        // True if there was a script error while rendering
-
-		// If true, previous rendering failed with a script problem, so we don't keep re-running until the script is fixed & re-run.
-		public bool HasScriptError => bScriptExecutionError || bScriptRenderError;
 
 		// Renderer representation of the scene we're currently rendering
 		private RenderScene _renderScene;
