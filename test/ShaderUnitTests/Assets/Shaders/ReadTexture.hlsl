d345cea4253392ad490fe9ecd4bb725e1a27d028
Simon Taylor <simontaylor81@hotmail.com>
2016-03-13 11:01:11 +0100

    Added some simple texture tests

---
+++ test/ShaderUnitTests/Assets/Shaders/ReadTexture.hlsl
@@ -0,0 +1,13 @@
+﻿// Simple compute shader that reads the contents of a texture.
+Texture2D<float4> Texture;
+RWStructuredBuffer<float4> OutBuffer;
+
+// Not efficient workgroup size, but we're running on WARP so who cares, this is simpler.
+[numthreads(1, 1, 1)]
+void Main(uint3 id : SV_DispatchThreadID)
+{
+	uint width, height;
+	Texture.GetDimensions(width, height);
+
+	OutBuffer[id.y * width + id.x] = Texture.Load(uint3(id.xy, 0));
+}
---
+++ test/ShaderUnitTests/TextureTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ShaderUnit;
+using ShaderUnit.Interfaces;
+using ShaderUnit.Interfaces.Shader;
+
+namespace ShaderUnitTests
+{
+	public class TextureTests : RenderTestBase
+	{
+		private IComputeHarness _testHarness;
+
+		[SetUp]
+		public void Setup()
+		{
+			_testHarness = CreateComputeHarness();
+		}
+
+		private IEnumerable<Vector4> ReadTexture(IShaderResource texture, int width, int height)
+		{
+			// Dispatch CS to read texture contents.
+			var cs = _testHarness.RenderInterface.CompileShader("ReadTexture.hlsl", "Main", "cs_4_0");
+			cs.FindResourceVariable("Texture").Set(texture);
+			return _testHarness.DispatchToBuffer<Vector4>(cs, "OutBuffer", Tuple.Create(width, height, 1), Tuple.Create(1, 1, 1));
+		}
+
+		[Test]
+		public void TextureFromEnumerable_RGBA32()
+		{
+			// Create texture with constant colour.
+			var texture = _testHarness.RenderInterface.CreateTexture2D(4, 4, Format.R8G8B8A8_UNorm, EnumerableEx.Repeat(0x000000FF, 16));
+			var results = ReadTexture(texture, 4, 4);
+
+			var expected = EnumerableEx.Repeat(new Vector4(1, 0, 0, 0), 16);
+			Assert.That(results, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void TextureFromFile()
+		{
+			// Load texture from file.
+			var texture = _testHarness.RenderInterface.LoadTexture("Images/red.png");
+			var results = ReadTexture(texture, 16, 16);
+
+			var expected = EnumerableEx.Repeat(new Vector4(1, 0, 0, 1), 16 * 16);
+			Assert.That(results, Is.EqualTo(expected));
+		}
+	}
+}
