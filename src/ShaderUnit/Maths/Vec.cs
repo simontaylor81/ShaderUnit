7b24d9ad1d6bbe170830b1ae4e09906ef7102339
Simon Taylor <simontaylor81@hotmail.com>
2016-03-13 17:23:06 +0100

    Added a very simple generics vector type
    No maths operations, just for passing data to and from the rendering
    systems. Uses T4 template to generate 2, 3 and 4 component versions
    since C# does have numeric generic parameters.

---
+++ src/ShaderUnit/Maths/Vec.cs
@@ -0,0 +1,46 @@
+﻿namespace ShaderUnit.Maths
+{
+	public struct Vec2<T>
+	{
+		public T x;
+		public T y;
+
+		public Vec2(T x, T y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	public struct Vec3<T>
+	{
+		public T x;
+		public T y;
+		public T z;
+
+		public Vec3(T x, T y, T z)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
+
+	public struct Vec4<T>
+	{
+		public T x;
+		public T y;
+		public T z;
+		public T w;
+
+		public Vec4(T x, T y, T z, T w)
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.w = w;
+		}
+	}
+
+}
+
---
+++ test/ShaderUnitTests/MathsTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ShaderUnit.Maths;
+
+namespace ShaderUnitTests
+{
+	// Tests for the vector and matrix structs.
+	public class MathsTests
+	{
+		[TestCase(1, 2)]
+		[TestCase(1.0f, 2.0f)]
+		[TestCase(1u, 2u)]
+		[TestCase(1.0, 2.0)]
+		public void Vec2ConstructorTest<T>(T x, T y) where T : struct
+		{
+			var vec = new Vec2<T>(x, y);
+			Assert.That(vec.x, Is.EqualTo(x));
+			Assert.That(vec.y, Is.EqualTo(y));
+		}
+
+		[TestCase(1, 2, 3)]
+		[TestCase(1.0f, 2.0f, 3.0f)]
+		[TestCase(1u, 2u, 3u)]
+		[TestCase(1.0, 2.0, 3.0)]
+		public void Vec3ConstructorTest<T>(T x, T y, T z) where T : struct
+		{
+			var vec = new Vec3<T>(x, y, z);
+			Assert.That(vec.x, Is.EqualTo(x));
+			Assert.That(vec.y, Is.EqualTo(y));
+			Assert.That(vec.z, Is.EqualTo(z));
+		}
+
+		[TestCase(1, 2, 3, 4)]
+		[TestCase(1.0f, 2.0f, 3.0f, 4.0f)]
+		[TestCase(1u, 2u, 3u, 4u)]
+		[TestCase(1.0, 2.0, 3.0, 4.0)]
+		public void Vec4ConstructorTest<T>(T x, T y, T z, T w) where T : struct
+		{
+			var vec = new Vec4<T>(x, y, z, w);
+			Assert.That(vec.x, Is.EqualTo(x));
+			Assert.That(vec.y, Is.EqualTo(y));
+			Assert.That(vec.z, Is.EqualTo(z));
+			Assert.That(vec.w, Is.EqualTo(w));
+		}
+	}
+}
