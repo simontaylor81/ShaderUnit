8f1658aec92d72ccda9db723d6faa5b6c4737a6d
Simon Taylor <simontaylor81@hotmail.com>
2016-03-13 19:50:45 +0100

    Tests for non-vec4-aligned matrices

--- test/ShaderUnitTests/Assets/Shaders/TypeTests.hlsl
+++ test/ShaderUnitTests/Assets/Shaders/TypeTests.hlsl
@@ -17,6 +17,10 @@ int2 Ret_uint2() { return uint2(1, 2); }
 int3 Ret_uint3() { return uint3(1, 2, 3); }
 int4 Ret_uint4() { return uint4(1, 2, 3, 4); }
 
+float3x3 Ret_float3x3() { return float3x3(1, 2, 3, 4, 5, 6, 7, 8, 9); }
+float3x3 Ret_int3x3() { return int3x3(1, 2, 3, 4, 5, 6, 7, 8, 9); }
+float3x3 Ret_uint3x3() { return uint3x3(1, 2, 3, 4, 5, 6, 7, 8, 9); }
+
 float4x4 Ret_float4x4() { return float4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16); }
 float4x4 Ret_int4x4() { return int4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16); }
 float4x4 Ret_uint4x4() { return uint4x4(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16); }
@@ -37,6 +41,10 @@ float In_int4(int4 param) { return (float)param.w; }
 float In_uint2(uint2 param) { return (float)param.y; }
 float In_uint3(uint3 param) { return (float)param.z; }
 float In_uint4(uint4 param) { return (float)param.w; }
+
+float In_float3x3(float3x3 param) { return param._21; }
+float In_int3x3(int3x3 param) { return (float)param._21; }
+float In_uint3x3(uint3x3 param) { return (float)param._21; }
 
 float In_float4x4(float4x4 param) { return param._24; }
 float In_int4x4(int4x4 param) { return (float)param._24; }
